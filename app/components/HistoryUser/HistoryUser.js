7bd74cc1efd25d4bc3378a4c240950d1e07bf7a5
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2018-11-28 21:58:31 +0700

    Jait API Balance in  Benefits category

--- app/components/HistoryUser/HistoryUser.js
+++ app/components/HistoryUser/HistoryUser.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import * as Core from '../../core';
 
-const HistoryUser = () => (
-  <View>
-    <View style={styles.sectionHeader}>
-      <View style={styles.contentHeader}>
-        <View />
-        <Image style={styles.imageHeader} />
-        <View />
+class HistoryUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Balance: '0',
+      Full_name: '',
+    };
+  }
+
+  componentWillMount() {
+    this.getUserBalance();
+    this.getUserDetail();
+  }
+
+  getUserBalance() {
+    Core.GetBalance((error, result) => {
+      data =
+        typeof result.data == 'string' ? JSON.parse(result.data) : result.data;
+      console.warn(data);
+      this.setState({
+        Balance: data.balance,
+      });
+    });
+  }
+
+  getUserDetail() {
+    Core.UserDetail((error, result) => {
+      data =
+        typeof result.data == 'string' ? JSON.parse(result.data) : result.data;
+      console.warn(data);
+      this.setState({
+        Full_name: data.profile.full_name,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.sectionHeader}>
+          <View style={styles.contentHeader}>
+            <View />
+            <Image style={styles.imageHeader} />
+            <View />
+          </View>
+        </View>
+        <View style={styles.sectionDetail}>
+          <Text
+            numberOfLines={2}
+            adjustsFontSizeToFit={true}
+            style={styles.amount}
+          >
+            Total Amount: S$ {this.state.Balance}
+          </Text>
+          <Text
+            numberOfLines={2}
+            adjustsFontSizeToFit={true}
+            style={styles.detail}
+          >
+            In-Network
+          </Text>
+        </View>
       </View>
-    </View>
-    <View style={styles.sectionDetail}>
-      <Text numberOfLines={2} adjustsFontSizeToFit={true} style={styles.amount}>
-        Total Amount: S$ 80.00
-      </Text>
-      <Text numberOfLines={2} adjustsFontSizeToFit={true} style={styles.detail}>
-        In-Network
-      </Text>
-    </View>
-  </View>
-);
+    );
+  }
+}
 
 export default HistoryUser;
