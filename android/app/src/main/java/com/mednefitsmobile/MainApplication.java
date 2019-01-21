b095964c18570c4bc7298af537634c5fd69317b1
detatsatrio <detatsatrio@Macs-MacBook-Air.local>
2019-01-21 13:16:56 +0700

    update android files

--- android/app/src/main/java/com/mednefitsmobile/MainApplication.java
+++ android/app/src/main/java/com/mednefitsmobile/MainApplication.java
@@ -3,12 +3,14 @@ package com.mednefitsmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,10 +30,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RNCameraPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+            new ReactNativeDocumentPicker()
       );
     }
 
