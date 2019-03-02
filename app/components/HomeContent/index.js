28d1a533e41340ca67139076508fd295b59e41e2
dhonaldJC <dhonaldjohannes@gmail.com>
2019-03-02 16:27:45 +0700

    update home search

--- app/components/HomeContent/index.js
+++ app/components/HomeContent/index.js
@@ -2,5 +2,6 @@ import HomeContent from './HomeContent';
 import MenuSide from './MenuSide';
 import styles from './styles';
 import HomeContentStatic from './HomeContentStatic';
+import SearchHome from './SearchHome'
 
-export { HomeContent, HomeContentStatic, MenuSide, styles };
+export { HomeContent, HomeContentStatic, MenuSide, styles, SearchHome };
