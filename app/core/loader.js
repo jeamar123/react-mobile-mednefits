db063abac78a68a6d6749a7947508d787ae0c257
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2019-03-30 22:17:31 +0700

    update wallet revision

--- app/core/loader.js
+++ app/core/loader.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import {View,  Dimensions, StatusBar, ActivityIndicator} from 'react-native'
+import { View, Dimensions, StatusBar, ActivityIndicator } from 'react-native'
 import Modal from "react-native-modal";
 import Texti from "../components/common/Texti"
 const { height, width } = Dimensions.get('window');
 
 const Loader = ({ isVisible }) => {
-	if(isVisible) {
+  if (isVisible) {
     return (
       <View>
         <Modal
@@ -15,16 +15,16 @@ const Loader = ({ isVisible }) => {
             alignItems: 'center'
           }}
         >
-          <ActivityIndicator color="#fff" size="large" />
+          <ActivityIndicator color="#0392cf" size="large" />
           <Texti
             fontColor="#FFFFFF"
-            >Just a sec...</Texti>
+          >Just a sec...</Texti>
         </Modal>
       </View>
     );
-	} else {
-		return null;
-	}
+  } else {
+    return null;
+  }
 };
 
 export { Loader };
