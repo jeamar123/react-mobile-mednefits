cc093f9af6e59948ea75af752183b833e5d2084f
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2019-08-18 20:17:21 +0700

    update text redirect before to dbs site

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -553,10 +553,7 @@ class Home extends Component {
           </Common.Texti>
           <Common.Texti
             fontColor="#FFFFFF"
-          >environment.</Common.Texti>
-          <Common.Texti
-            fontColor="#FFFFFF"
-          >...</Common.Texti>
+          >Bank environment.</Common.Texti>
         </Modal>
       </View>
     );
