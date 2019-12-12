3063b821ec195c404a2940426879ba328fa6fb7a
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Air.local>
2019-12-12 10:58:25 +0800

    updates

--- app/screens/HistoryAfterUpload.js
+++ app/screens/HistoryAfterUpload.js
@@ -106,12 +106,12 @@ class HistoryAfterUpload extends Component {
         paid_by_credits_myr: data.paid_by_credits_converted,
         paid_by_cash_myr: data.paid_by_cash_converted,
 
-        total_amount: data.total_amount,
-        cap_per_visit: data.cap_per_visit,
-        bill_amount: data.bill_amount,
-        consultation_fee: data.consultation_fee,
-        paid_by_credits: data.paid_by_credits,
-        paid_by_cash: data.paid_by_cash,
+        total_amount: data.default_currency.toUpperCase() == 'SGD' ? data.total_amount : data.total_amount_converted,
+        cap_per_visit: data.default_currency.toUpperCase() == 'SGD' ? data.cap_per_visit : data.cap_per_visit_converted,
+        bill_amount: data.default_currency.toUpperCase() == 'SGD' ? data.bill_amount : data.bill_amount_converted,
+        consultation_fee: data.default_currency.toUpperCase() == 'SGD' ? data.consultation_fee : data.consultation_fee_converted,
+        paid_by_credits: data.default_currency.toUpperCase() == 'SGD' ? data.paid_by_credits : data.paid_by_credits_converted,
+        paid_by_cash: data.default_currency.toUpperCase() == 'SGD' ? data.paid_by_cash : data.paid_by_cash_converted,
 			})
 		})
 	}
